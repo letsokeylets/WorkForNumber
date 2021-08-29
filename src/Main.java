b601c8976b2827d57fb4dea861cb3439e5cb578b
Mozdir Taras <letsokey@mail.ru>
2021-08-29 13:15:17 +0400

    fix main class 29.08.2021

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,28 @@ public class Main {
 
     //Класс без использования библеотеки Stream API
     public static void main(String[] args) {
-        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        List<Integer> newList = new ArrayList<>();
-        for (Integer value : intList) {
-            if (value > 0 && value % 2 == 0) {
-                newList.add(value);
+        //Получим новый массив только с положительными числами кратными 2
+        int[] intList = {1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4};
+        int[] newList = new int[intList.length];
+        for (int i = 0; i < intList.length; i++) {
+            int number = intList[i];
+            if (number > 0 && number % 2 == 0) {
+                newList[i] = number;
             }
         }
-        newList.sort(Comparator.naturalOrder());
-        System.out.println(newList);
+        //Отсортируем полученный массив по возрастанию
+        for (int i = 0; i < newList.length; i++) {
+            for (int j = 0; j < newList.length-1; j++) {
+                int number = newList[i];
+                if (number < newList[j]) {
+                    newList[i] = newList[j];
+                    newList[j] = number;
+                }
+            }
+        }
+        //Вывод на экран отсортированного массива, убрав лишние элементы массива (нули)
+        for (int i : newList) {
+            if (i != 0) System.out.print(i + " ");
+        }
     }
 }
